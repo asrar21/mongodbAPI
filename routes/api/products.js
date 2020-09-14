ab51baccc8b42c468f4f05aacd999500c1e2189f
asrar21 <alamsyedmohammadasrar@gmail.com>
2020-09-14 19:08:21 +0500

    made full api

--- routes/api/products.js
+++ routes/api/products.js
@@ -3,6 +3,7 @@ const router = express.Router();
 let ObjectId =require("mongodb").ObjectId
 
 const Product = require('../../model/Product.js');
+const User = require('../../model/User');
 router.get(
     '/',
     
@@ -35,15 +36,26 @@ router.get(
       try {
         // console.log("req.query.createdBy",req.query.createdBy)
        
-        
-       let product=await Product.find()
+      
        
-       if(product){
-          res.json({"message":`success`,"data":product,"status":200});
-       }
-       else{
-          res.json({"message":"data not found","data":[],"status":400});
-       }
+       let  response=await Product.find()
+       // console.log(result);
+ 
+  let result=[]
+     // console.log("response",response)
+ for(let i=0;i<response.length;i++){
+     let id=ObjectId(response[i].createdBy)
+     let userInfo=await User.find({_id:id})
+     // console.log(userInfo)
+     result.push({"productid":response[i]._id  ,"name":response[i].name,"price":response[i].price,"location":response[i].location,"contact":userInfo[0].cellnumber,"sellerName":userInfo[0].name,"image":response[i].image})
+     // console.log("result",result)
+}
+if(result!==[]){
+     res.json({"message":`success`,"data":result,"status":200});
+  }
+  else{
+     res.status(500).send({"message":"query not Running","data":[],"status":400});
+  }
       } catch (err) {
         res.json({"message":`${err}`,"status":400});
       }
