e3cc965ab30045eacc9f9fc5089a98a13f4dc3b4
asrar21 <alamsyedmohammadasrar@gmail.com>
2022-01-02 21:17:26 -0900

    resolved cors issues

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 // const path = require('path');
+const cors =require('cors');
 const bodyParser = require("body-parser");
 const app = express();
 
@@ -9,15 +10,16 @@ connectDB();
 
 // Init Middleware
 // app.use(express.json());
+app.use(cors());
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
-app.use(function (req, res, next) {
-    //Enabling CORS 
-    res.header("Access-Control-Allow-Origin", "*");  
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization, api-token, x-channel");
-    next();
-  });
+// app.use(function (req, res, next) {
+//     //Enabling CORS 
+//     res.header("Access-Control-Allow-Origin", "*");  
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization, api-token, x-channel");
+//     next();
+//   });
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
